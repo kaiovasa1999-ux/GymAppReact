2736daa2ea23b16ed23abe3a5952dd4dc71fb84f
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-16 15:10:35 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Box } from '@mui/material';
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import Home from './Pages/Home';
 import ExerciseDetail from './Pages/ExerciseDetail';
 
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './App';
+import App from './App'
 
-const root = ReactDOM.creatRoot(document.getElementsById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App />
+    <App />
     </BrowserRouter>
   );
-  
-
