3254e42f7d293808fd8e3c6d3740a147927e5bf3
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-17 14:53:12 +0300

    fetch the data from RapidAPI

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,14 +1,14 @@
-const options = {
+export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_KEY,
-    }
+    },
   };
-export const fetchData=async(url,options) =>{
-    const response = await fetch(url,options);
-    const data =await response.json();
-
-
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
     return data;
-}
+  };
